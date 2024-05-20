1b24f4231e554dd2d809efc7feec5388a1135cdb
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-05-20 19:54:48 -0300

    cambio de codigo

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    FILE *Archivo;
-    Archivo = fopen("output.txt", "w");
-    if (Archivo)
-    {
-      fprintf(Archivo, "Hello, World!");
-    }else{
-      printf("Error al abrir el archivo");
-    }
-     return 0;  
+int main() { 
+    printf("Hello, World!");
+    return 0;  
 }
